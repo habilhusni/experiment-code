dcfd1fd89fa21a6b01b46ce9ca60495f84b4043b
habilhusni <you@example.com>
2017-04-29 07:26:44 +0700

    adding-some-experiment

---
+++ routing-vue/coba-routing/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view></router-view> -->
+    <header class="app-header">
+      <app-nav></app-nav>
+    </header>
+    <main class="container">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+
+import AppNav from './components/AppNav';
+
+export default {
+  components: {
+    AppNav
+  }
+}
+
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ routing-vue/coba-routing/src/components/AppNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-full navbar-light navbar-jw">
+    <a class="navbar-brand" href="#">
+      <!-- <img src="../assets/jay.svg" alt="Jayway logo" /> Jaystore -->
+    </a>
+    <div class="nav navbar-nav">
+      <router-link
+        to="home"
+        class="nav-item nav-link"
+        active-class="active"
+        exact
+      >
+        Home
+      </router-link>
+      <router-link
+        to="manage-products"
+        class="nav-item nav-link"
+        active-class="active"
+        exact
+      >
+        Manage Products
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .navbar-nav {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
